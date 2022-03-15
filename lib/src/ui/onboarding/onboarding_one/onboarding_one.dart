4d9665e6fd20ad0fd552947633be558d83f27260
Miles <clearhackerashi@gmail.com>
2022-03-15 14:40:59 +0500

    Initial commit

---
+++ lib/src/ui/onboarding/onboarding_one/onboarding_one.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OnboardingOneScreen extends StatefulWidget {
+  const OnboardingOneScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingOneScreen> createState() => _OnboardingOneScreenState();
+}
+
+class _OnboardingOneScreenState extends State<OnboardingOneScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
