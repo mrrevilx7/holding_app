d50ac7845124243aaf41285f2e6c05a1ca7d5d0c
Miles <clearhackerashi@gmail.com>
2022-03-16 13:06:21 +0500

    hhh6

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -6,6 +6,7 @@ class AppTheme {
   static const Color neturalBlue = Color(0xFF354B91);
   static const Color lightgray = Color(0xFFC4C4C4);
   static const Color mainlyBLUE  = Color(0xFFBCADCA);
+  static const Color gray  = Color(0xFFB8B8B8);
 
 
 
--- lib/src/ui/auth/login/login_screen.dart
+++ lib/src/ui/auth/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:holding_app/src/theme/app_theme.dart';
 
 import '../../../utils/utils_screen.dart';
@@ -11,11 +12,15 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _usernameCon = TextEditingController();
+  TextEditingController _passwordCon = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     double w = Utils.getWidth(context);
     double h = Utils.getHeight(context);
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppTheme.black,
       body: Stack(
         children: [
@@ -52,6 +57,109 @@ class _LoginScreenState extends State<LoginScreen> {
                     fontFamily: 'Avenir',
                     color: AppTheme.mainlyBLUE,
                   ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Holding",
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                  fontFamily: 'Avenir',
+                  color: AppTheme.mainlyBLUE,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 120 * h,
+                ),
+                width: 375 * w,
+                height: 467 * h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(32),
+                    topLeft: Radius.circular(32),
+                  ),
+                  color: AppTheme.white,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 24 * w,
+                        right: 24 * w,
+                        top: 40 * h,
+                      ),
+                      child: Center(
+                          child: Text(
+                        "Eng yaxshi hamkorlik",
+                        style: TextStyle(
+                          fontSize: 30,
+                          letterSpacing: 1.2,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Avenir',
+                          color: AppTheme.black,
+                        ),
+                      )),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 24,
+                        top: 68 * h,
+                        right: 24,
+                      ),
+                      width: double.infinity,
+                      height: 52,
+                      child: TextField(
+                        controller: _usernameCon,
+                        decoration: InputDecoration(
+                            labelText: "Username",
+                            labelStyle: TextStyle(
+                              color: AppTheme.gray,
+                              fontFamily: 'Avenir',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18,
+                              // letterSpacing: 0.17
+                            )),
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Avenir',
+                          color: AppTheme.black,
+                          // letterSpacing: - 0.1,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 24,
+                        top: 32,
+                        right: 24,
+                      ),
+                      width: double.infinity,
+                      height: 52,
+                      child: TextField(
+                        controller: _passwordCon,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                              color: AppTheme.gray,
+                              fontFamily: 'Avenir',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18,
+                              // letterSpacing: 0.17
+                            ),
+                        ),
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Avenir',
+                          color: AppTheme.black,
+                          // letterSpacing: - 0.1,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
